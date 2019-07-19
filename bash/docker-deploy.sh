c4f9a69f10fd9f23838216498ef4dd92fdb1521d
yangtopia <yangtopia@gmail.com>
2019-07-20 03:14:14 +0900

    도커 + 트레비스 설정

--- bash/docker-deploy.sh
+++ bash/docker-deploy.sh
@@ -1,3 +1,7 @@
+pushd ../selfdating-web
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker build -t yangtopia/selfdating-web .
 
 docker push yangtopia/selfdating-web
