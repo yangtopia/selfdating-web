780a4b9ef77be3355d63701d29a50d330559dc00
yangtopia <yangtopia@gmail.com>
2019-07-20 17:43:21 +0900

    zeit serveless 호스팅 관련 코드 제거

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true,
-  target: 'serverless',
   webpack(config, options) {
     config.resolve.alias = Object.assign(config.resolve.alias, { 'react-dom': '@hot-loader/react-dom' });
     return config;
