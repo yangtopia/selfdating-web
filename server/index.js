ace33544d3e2d921e8f65b2702012224f2cbb1a3
yangtopia <yangtopia@gmail.com>
2019-07-21 00:58:01 +0900

    바이럴페이지 커스텀 라우팅 추가

--- server/index.js
+++ server/index.js
@@ -9,12 +9,21 @@ const dev = ['test', 'production'].indexOf(process.env.NODE_ENV) === -1;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const _nextRoute = (server, path, nextPath, addedQuery = {}) => {
+  server.get(path, (req, res) => {
+    const { params = {}, query = {} } = req;
+    return app.render(req, res, nextPath, { ...query, ...params, ...addedQuery });
+  });
+};
+
 app.prepare().then(() => {
   const server = express();
   // Static 파일 중 서버 루트에 반드시 있어야 하는 것은 /static/_root 에 넣는다.
   // "/mainfest.json", "/tas-sw.js" 등...
   server.use(serveStatic(path.join(__dirname, '../static/_root')));
   if (!dev) server.use(compression());
+
+  _nextRoute(server, '/viral/:id', '/');
 
   server.get('*', (req, res) => {
     return handle(req, res);
