d7bdc71f3640623636148f3b39f519923c968cb8
yangtopia <yangtopia@gmail.com>
2019-07-20 21:44:28 +0900

    favicon 및 menifest 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 import 'moment/locale/ko';
 import '../styles/styles.scss';
 
@@ -19,6 +20,13 @@ class MyApp extends App {
 
     return (
       <Container>
+        <Head>
+          <title>셀프소개팅</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <Component {...pageProps} />
       </Container>
     );
